8f8c3f30a526e4a188e11bdf06eeeee324016e42
Luigi Pugliese <crackednut00@outlook.com>
2017-12-16 14:58:48 +0100

    amend

--- MusescoreToPitches/minidomed.py
+++ MusescoreToPitches/minidomed.py
@@ -1,6 +1,6 @@
 from xml.dom import minidom
 
-file = 'C:\\Users\\crack\\Documents\\GithubProjects\\PythonProj\\MusescoreToPitches\\data\\asd.xml'
+file = 'C:\\Users\\crack\\Documents\\GithubProjects\\Python\\MusescoreToPitches\\data\\asd.xml'
 
 notesDict = {60: 261, 61: 277, 62: 293, 63: 311, 64: 329, 65: 349, 66: 370, 67: 392, 68: 415, 69: 440, 70: 466, 71: 494,
              72: 523, 73: 554, 74: 587, 75: 622, 76: 659, 77: 698, 78: 740, 79: 784, 80: 830, 81: 880}
